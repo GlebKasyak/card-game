752f880b46df41985f818f279aae59400661b08d
Gleb <gkebkasyak@gmail.com>
2020-01-28 15:31:56 +0300

    Updates

---
+++ precache-manifest.e64939f683f3944655e18430656fc9f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d05d12b19f55ebd726d14d816e0ad6",
+    "url": "/card-game/index.html"
+  },
+  {
+    "revision": "9a14832016e381dafd65",
+    "url": "/card-game/static/css/main.cd0580ab.chunk.css"
+  },
+  {
+    "revision": "55289490e0cc5699268c",
+    "url": "/card-game/static/js/2.379044c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/card-game/static/js/2.379044c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a14832016e381dafd65",
+    "url": "/card-game/static/js/main.d8bf34f2.chunk.js"
+  },
+  {
+    "revision": "1c2ede4b3bc6ffe222d5",
+    "url": "/card-game/static/js/runtime-main.9d11bcae.js"
+  },
+  {
+    "revision": "7c811eda64ffe6014f989d352cd2ebad",
+    "url": "/card-game/static/media/cloth.7c811eda.jpg"
+  },
+  {
+    "revision": "06218d399fbc5729b85db240d23b78e5",
+    "url": "/card-game/static/media/images.06218d39.png"
+  }
+]);
